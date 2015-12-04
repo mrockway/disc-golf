a79d0360fe855803d9da33f998fac9af708f3ccd
mrockway <mrockway@gmail.com>
2015-12-03 16:06:35 -0800

    map on page with example points

--- public/profile.js
+++ public/profile.js
@@ -54,6 +54,9 @@ $(function() {
 			});
 		}
 
+		// draw map based on course results
+		createMap(courseData);
+
 		// Append course results to the page
 		$('.coursesDiv').append(courseHTML);
 		$('.courseDetailsDiv').hide();
@@ -101,5 +104,47 @@ $(function() {
 		}
 		return arr;
 	}
+
+	// mapbox creation
+	function createMap(courseData) {
+		L.mapbox.accessToken = 'pk.eyJ1IjoibXJvY2t3YXkiLCJhIjoiY2locW1mMGo3MDRwcXVybHhhMXRrbXU1MyJ9.-5Z2oWSNPlsLGMP_5_mMog';
+		var map = L.mapbox.map('map', 'mapbox.emerald').setView([37.8, -96], 4);
+		var myLayer = L.mapbox.featureLayer().addTo(map);
+		var geojson = [
+  {
+    "type": "Feature",
+    "geometry": {
+      "type": "Point",
+      "coordinates": [-77.031952,38.913184]
+    },
+    "properties": {
+      "title": "Mapbox DC",
+      "description": "1714 14th St NW, Washington DC",
+      "marker-color": "#3ca0d3",
+      "marker-size": "large",
+      "marker-symbol": "rocket"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+      "type": "Point",
+      "coordinates": [-122.413682,37.775408]
+    },
+    "properties": {
+      "title": "Mapbox SF",
+      "description": "155 9th St, San Francisco",
+      "marker-color": "#63b6e5",
+      "marker-size": "large",
+      "marker-symbol": "rocket"
+    }
+  }
+];
+myLayer.setGeoJSON(geojson);
+	}
+	
+
+	// end mapbox
+
 });
 
